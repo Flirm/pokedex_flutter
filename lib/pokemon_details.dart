6854ad2b8baa2e7cfb951374c767638cc169a42c
Francisco de Lima Ramos <121270101+Flirm@users.noreply.github.com>
2025-10-30 20:22:31 -0300

    placeholders

---
+++ lib/pokemon_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PokemonDetails extends StatefulWidget {
+  const PokemonDetails({super.key});
+
+  @override
+  State<PokemonDetails> createState() => _PokemonDetailsState();
+}
+
+class _PokemonDetailsState extends State<PokemonDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
